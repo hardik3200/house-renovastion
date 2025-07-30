6174c736f5d98789bccdebf71deff95f900fc441
my work <hdabhi287@gmail.com>
2025-07-30 16:29:26 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+
+        
+
+
+
+
+
+ $('.last-section .owl-carousel').owlCarousel({
+            loop:true,
+            margin:20,
+            nav:true,
+            autoplay:true,
+            autoplayspeed:500,
+            items:6,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:6
+                }
+            }
+         });
+         
+         
+         
+         
+
+
+$('.banner-section .owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            autoplay:true,
+            autoplayspeed:2000,
+            items:1,
+            
+         })
+         
+         
+         
+         $('.middle-section .owl-carousel').owlCarousel({
+            loop:true,
+            margin:20,
+            nav:true,
+            autoplay:true,
+            autoplayspeed:3000,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+         });
+
